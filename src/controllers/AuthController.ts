999688908ad8020d5018540a9bd385e5b7b7acab
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-09-26 21:47:32 +0530

    some fixes in auth conrteoller file

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,5 @@
-import { request, response } from 'express'
+import { Request, Response } from 'express';
 import prisma from '../config/db.config.js';
-
 
 interface LoginPayloadType {
 	name: string;
@@ -9,22 +8,23 @@ interface LoginPayloadType {
 	oauth_id: string;
 	image: string;
 }
+
 class AuthController {
-	static async login(request: Request, response: Response) {
+	static async login(request: Request<{}, {}, LoginPayloadType>, response: Response) {
 		try {
 			const body: LoginPayloadType = request.body;
 			let findUser = await prisma.user.findUnique({
 				where: {
 					email: body.email
 				}
-			}
-			)
+			});
+			console.log(findUser);
+			// Add your logic here to handle the user login
 		} catch (error) {
-
+			console.error(error);
+			response.status(500).json({ error: 'Internal Server Error' });
 		}
-
 	}
-};
-
+}
 
 export default AuthController;
