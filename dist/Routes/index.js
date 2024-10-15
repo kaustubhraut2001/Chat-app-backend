2aa83eefa7cf776ccef02c1ce15cda7ff2ce3db5
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-10-15 22:53:58 +0530

    chat api testing and creating done

--- dist/Routes/index.js
+++ dist/Routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
+import authMiddleware from "../middlewares/AuthMiddleware.js";
+import ChatGroupController from "../controllers/ChatGroupController.js";
 const router = Router();
 router.post("/auth/login", AuthController.login);
+router.post("/chat-group", authMiddleware, ChatGroupController.store);
 export default router;
