d9cdd9d1bae3c16b8a29deda2277db0a255afc7c
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-10-17 22:51:57 +0530

    socket

--- dist/socket.js
+++ dist/socket.js
@@ -1,6 +1,10 @@
 export function setupSocket(io) {
     io.on("connect", (socket) => {
         console.log("Socket connected", socket.id);
+        socket.on("message", (data) => {
+            console.log("Received message from user: ", data);
+            socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+        });
         socket.on("disconnect", () => {
             console.log("User is disconnected", socket.id);
         });
--- src/socket.ts
+++ src/socket.ts
@@ -3,6 +3,10 @@ import { Server } from "socket.io";
 export function setupSocket(io: Server) {
   io.on("connect", (socket) => {
     console.log("Socket connected", socket.id);
+    socket.on("message", (data) => {
+      console.log("Received message from user: ", data);
+      socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+    });
     socket.on("disconnect", () => {
       console.log("User is disconnected", socket.id);
     });
