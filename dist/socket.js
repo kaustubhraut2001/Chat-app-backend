6a55e0ee6eac2fabda288a54825e3fa4e9af2bb8
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-10-29 15:19:15 +0530

    socket io changes

--- dist/socket.js
+++ dist/socket.js
@@ -1,9 +1,19 @@
 export function setupSocket(io) {
+    io.use((socket, next) => {
+        const room = socket.handshake.auth.room || socket.handshake.room;
+        if (!room) {
+            return next(new Error("Invalid Room Please pass room Id"));
+        }
+        socket.room = room;
+        next();
+    });
     io.on("connect", (socket) => {
+        socket.join(socket.room);
         console.log("Socket connected", socket.id);
         socket.on("message", (data) => {
             console.log("Received message from user: ", data);
-            socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+            // socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+            io.to(socket.room).emit("message", data);
         });
         socket.on("disconnect", () => {
             console.log("User is disconnected", socket.id);
--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,26 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
+interface CustomSocket extends Socket {
+  room?: string;
+}
 export function setupSocket(io: Server) {
-  io.on("connect", (socket) => {
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room || socket.handshake.room;
+    if (!room) {
+      return next(new Error("Invalid Room Please pass room Id"));
+    }
+    socket.room = room;
+    next();
+  });
+
+  io.on("connect", (socket: CustomSocket) => {
+    socket.join(socket.room);
+
     console.log("Socket connected", socket.id);
     socket.on("message", (data) => {
       console.log("Received message from user: ", data);
-      socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+      // socket.broadcast.emit("message", data); // Broadcast the message to all connected users.
+      io.to(socket.room).emit("message", data);
     });
     socket.on("disconnect", () => {
       console.log("User is disconnected", socket.id);
